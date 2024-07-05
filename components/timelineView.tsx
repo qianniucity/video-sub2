1e34ecc8b44af94b5db65eb95cb29929f972994e
king101125s@163.com <king101125s@gmail.com>
2024-07-05 11:37:02 +0800

    add 音波添加字幕，编辑字幕

--- components/timelineView.tsx
+++ components/timelineView.tsx
@@ -1,8 +1,10 @@
 "use client"
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import WaveSurfer from 'wavesurfer.js';
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'
+
 interface PlayerProps {
   videoUrl: string;
 }
@@ -12,9 +14,8 @@ const TimelineView: React.FC<PlayerProps> = ({ videoUrl }) => {
   const videoRef = useRef<HTMLVideoElement>(null); // 创建一个ref来引用视频元素
   const waveSurferRef = useRef<WaveSurfer>(); // 用于引用WaveSurfer实例
   const waveContainerRef = useRef<HTMLDivElement>(null); // 用于引用波形容器
-
-
-
+  const [loop, setLoop] = useState(true)
+  const [zoom, setZoom] = useState(10); // 1. 添加一个新的状态
 
   useEffect(() => {
     if (videoRef.current && waveContainerRef.current) {
@@ -43,18 +44,64 @@ const TimelineView: React.FC<PlayerProps> = ({ videoUrl }) => {
       })
 
 
-      waveSurferRef.current = WaveSurfer.create({
+      const ws = waveSurferRef.current = WaveSurfer.create({
         container: waveContainerRef.current, // 使用专门的容器
-        waveColor: 'violet',
-        progressColor: 'purple',
+        waveColor: '#086280',
+        progressColor: '#806f77',
+        cursorColor: '#672fbc',
+        hideScrollbar: true,
         media: videoRef.current,
         url: videoUrl,
         plugins: [topTimeline, bottomTimeline],
       });
 
+      const wsRegions = ws.registerPlugin(RegionsPlugin.create())
+
+      // Create some regions at specific time ranges
+      ws.on('decode', () => {
+        // Regions
+        wsRegions.addRegion({
+          id: "1",
+          start: 0,
+          end: 5.0123123123,
+          content: 'Resize me',
+          resize: true,
+          channelIdx: 1,
+          contentEditable: true,
+        })
+        wsRegions.addRegion({
+          id: "2",
+          start: 9,
+          end: 12,
+          content: 'Cramped region12312313212323',
+          resize: true,
+          channelIdx: 1,
+          contentEditable: true,
+        })
+        wsRegions.addRegion({
+          id: "3",
+          start: 20,
+          end: 27,
+          content: 'Drag me',
+          resize: true,
+          channelIdx: 1,
+          contentEditable: true,
+        })
+      })
+
+      wsRegions.enableDragSelection({
+        color: 'rgba(255, 0, 0, 0.1)',
+      })
+
+      wsRegions.on('region-updated', (region) => {
+        console.log('Updated region', region)
+      })
+
       return () => waveSurferRef.current?.destroy();
     }
   }, [videoUrl]);
+
+  // TODO 添加音波伸缩
   return (
     <>
       <video
@@ -62,9 +109,17 @@ const TimelineView: React.FC<PlayerProps> = ({ videoUrl }) => {
         src={videoUrl}
         controls
         playsInline
-        style={{ width: '100%', maxWidth: '600px', margin: '0 auto', display: 'block' }}
-      />
+        style={{ width: '100%', maxWidth: '1200px', margin: '0 auto', display: 'block' }}
+      >
+        <track
+          src="/video/ap.vtt"
+          kind="subtitles"
+          srcLang="en"
+          label="English" />
+      </video>
       <div ref={waveContainerRef} style={{ width: '100%', margin: '20px auto' }} />
+
+
     </>
   );
 }
